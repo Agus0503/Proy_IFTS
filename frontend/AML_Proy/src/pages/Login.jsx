30a35c6716ff9c2dcc78409d8ff281256dacbc21
Agus0503 <agustin.ostua@hotmail.com.ar>
2024-11-21 21:33:35 -0300

    Update

--- frontend/AML_Proy/src/pages/Login.jsx
+++ frontend/AML_Proy/src/pages/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
   
   return (
     <div className="form-wrapper">      
-      <img ref={logoRef} className="logo" src="../../public/logo-circulo.svg" />
+      <img ref={logoRef} className="logo"/>
       <Form route="/api/token/" method="login" message="¡Bienvenido de nuevo!" />
     </div>
   );
